71a39e78ba219219fe6ba9bff954634c47881971
PawelRogowski <pawel.rogowski.praca@gmail.com>
2022-12-08 03:14:18 +0100

    fixed task7 fontSize default value

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,17 @@
-// get all li.item elements within the ul#categories
+// Get all li.item elements within the ul#categories
 const liItems = document.querySelectorAll("ul#categories li.item");
 
 console.log(`Number of categories: ${liItems.length}`);
 
-// use map to create an array of h2 elements within each item.
+// Use map to create an array of h2 elements within each item.
 const h2s = Array.from(liItems).map((item) => item.querySelector("h2"));
-
 /*
-  use forEach to iterate over the array and log the text content of each h2 element,
-  as well as the number of li elements within its parent li.item element. 
-  */
+Use forEach to iterate over the array and log the text content of each h2 element,
+as well as the number of li elements within its parent li.item element. 
+*/
 h2s.forEach((h2) => {
   const listItemNumber = h2.parentElement.querySelectorAll("li").length;
   console.log(`Category: ${h2.textContent}\nElements: ${listItemNumber}`);
 });
+
+// not sure if it's meant to be compacted into single consol.log()
--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
 // Select elements
 const inputElement = document.getElementById("font-size-control");
 const textElement = document.getElementById("text");
-
+// Set initial font size
+textElement.style.fontSize = `${inputElement.value}px`;
 // Add input event listener
 inputElement.addEventListener("input", () => {
   // Update text font-size on slider inpuit
