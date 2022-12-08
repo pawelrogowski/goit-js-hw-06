3411cfbce8cca2059ecb636f308f86608d08da02
PawelRogowski <pawel.rogowski.praca@gmail.com>
2022-12-08 04:16:24 +0100

    added some comments

--- js/task-02.js
+++ js/task-02.js
@@ -1,15 +1,22 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
-//search for ul#ingredients
+// Search for the ul#ingredients element on the page.
 const ul = document.querySelector("ul#ingredients");
 
-//create new array, which contains li elements created from ingredients array.
+// Create a new array of li elements, one for each ingredient in the ingredients array.
 const items = ingredients.map((ingredient) => {
+  // Create a new li element.
   const li = document.createElement("li");
+
+  // Set the text of the li element to the name of the ingredient.
   li.textContent = ingredient;
+
+  // Add the "item" class to the li element.
   li.classList.add("item");
+
+  // Return the li element.
   return li;
 });
 
-//append the list of li elements in the items array to the ul element.
+// Append the list of li elements in the items array to the ul element.
 ul.append(...items);
