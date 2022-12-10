cb58f86e5224da3c9a4120594f34610839c65535
PawelRogowski <pawel.rogowski.praca@gmail.com>
2022-12-10 20:59:06 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -9,17 +9,16 @@ const inputElement = document.querySelector("input");
 const createButton = document.querySelector("[data-create]");
 const destroyButton = document.querySelector("[data-destroy]");
 const boxesElement = document.querySelector("#boxes");
-
+// Define a function named "createBoxes" that takes an "amount" parameter
 function createBoxes(amount) {
-  // set the starting size
+  // Set the starting box size
   const lastBoxSize = boxesElement.lastChild ? boxesElement.lastChild.offsetWidth + 10 : 30;
-  // Define a function named "createBoxes" that takes an "amount" parameter
+  // Loop through specified number of times
   for (let i = 0; i < amount; i++) {
-    // Loop through the specified number of times
     // Create a "div" element
     const boxElement = document.createElement("div");
 
-    // Set the width and height of the "div" element to be a function of the loop index, and set its background color to a random hexadecimal value
+    // Set the width and height of the "div" element to be a function of the loop index, and set its background color to a random hex value
     boxElement.style.width = `${lastBoxSize + i * 10}px`;
     boxElement.style.height = `${lastBoxSize + i * 10}px`;
     boxElement.style.backgroundColor = getRandomHexColor();
@@ -30,17 +29,14 @@ function createBoxes(amount) {
 }
 
 function destroyBoxes() {
-  // Clear the HTML content of the "boxes" container
   boxesElement.innerHTML = "";
 }
 
 createButton.addEventListener("click", () => {
-  // When the "create" button is clicked, call the "createBoxes" function and pass the value of the input element as an argument
   createBoxes(inputElement.value);
 });
 
 // Add an event listener for the "click" event on the "destroy" button
 destroyButton.addEventListener("click", () => {
-  // When the "destroy" button is clicked, call the "destroyBoxes" function
   destroyBoxes();
 });
